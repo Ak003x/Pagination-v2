301eff3ad7325d249f78cf97b90e5a32f5620e30
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-20 13:07:03 +0530

    loading set

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 export default function App() {
-  return 
-  
-  (<div>
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  if (loading) return <h3>Please Wait Page is been Loading....</h3>;
 
-
-
-  </div>)
-  
+  return (
+    <div>
+      <h1>Simple Pagination</h1>
+    </div>
+  );
 }
